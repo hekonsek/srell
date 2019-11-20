68f400f818c07a655b16370a7f1a7a6bd10f30a5
hekonsek <hekonsek@gmail.com>
2019-11-20 14:41:50 +0100

    Slack token should be configurable.

--- srell.go
+++ srell.go
@@ -4,48 +4,58 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/nlopes/slack"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 func main() {
-	ListenAndResponse()
+	BotConnect()
 }
 
-func ListenAndResponse() {
-	api := slack.New("xoxb-826173663410-840819711239-vFyTDYDV592MZjZc2ANVDnRr", slack.OptionDebug(true))
+func BotConnect() {
+	slackToken := os.Getenv("SLACK_TOKEN")
+	if slackToken == "" {
+		println("SLACK_TOKEN cannot be empty.")
+		os.Exit(1)
+	}
+
+	api := slack.New(slackToken, slack.OptionDebug(true))
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 
 	for msg := range rtm.IncomingEvents {
 		switch msg.Data.(type) {
-			case *slack.UnmarshallingErrorEvent:
-				eventText := fmt.Sprintf("%+v\n", msg)
-				eventPayloadWithClosingBracketParts := strings.Split(eventText, "message\":")
-				if len(eventPayloadWithClosingBracketParts) != 2 {
-					println("Skipping....")
+		case *slack.UnmarshallingErrorEvent:
+			eventText := fmt.Sprintf("%+v\n", msg)
+			eventPayloadWithClosingBracketParts := strings.Split(eventText, "message\":")
+			if len(eventPayloadWithClosingBracketParts) != 2 {
+				println("Skipping....")
+			} else {
+				eventPayloadWithClosingBracket := eventPayloadWithClosingBracketParts[1]
+				eventPayload := eventPayloadWithClosingBracket[0 : len(eventPayloadWithClosingBracket)-2]
+				var m map[string]interface{}
+				err := json.Unmarshal([]byte(eventPayload), &m)
+				if err != nil {
+					println(err.Error())
 				} else {
-					eventPayloadWithClosingBracket := eventPayloadWithClosingBracketParts[1]
-					eventPayload := eventPayloadWithClosingBracket[0 : len(eventPayloadWithClosingBracket)-2]
-					var m map[string]interface{}
-					err := json.Unmarshal([]byte(eventPayload), &m)
-					if err != nil {
-						println(err.Error())
-					} else {
-						if strings.HasPrefix(m["text"].(string), "shell") {
-							commandParts := strings.Split(m["text"].(string), " ")
-							x := commandParts[1]
-							y := commandParts[2:]
-							cmd := exec.Command(x, y...)
-							out, err := cmd.CombinedOutput()
-							if err != nil {
-								println(err.Error())
-							} else {
-								rtm.SendMessage(&slack.OutgoingMessage{Type: "message", Channel: m["channel"].(string), Text: "```" + string(out) + "```" })
-							}
+					if strings.HasPrefix(m["text"].(string), "shell") {
+						commandParts := strings.Split(m["text"].(string), " ")
+						x := commandParts[1]
+						y := commandParts[2:]
+						cmd := exec.Command(x, y...)
+						if x == "cd" {
+							cmd.Dir = commandParts[2]
+						}
+						out, err := cmd.CombinedOutput()
+						if err != nil {
+							println(err.Error())
+						} else {
+							rtm.SendMessage(&slack.OutgoingMessage{Type: "message", Channel: m["channel"].(string), Text: "```" + string(out) + "```"})
 						}
 					}
 				}
+			}
 		}
 	}
 }
